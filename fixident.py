2cb647f8dabb952fc4320397013567fae5173a77
Jakob Holst <jho@miracle.dk>
2023-05-15 12:10:32 +0200

    demo

---
+++ fixident.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import re
+import sys
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+    # Use the filename variable for further processing
+    print("Filename:", filename)
+    with open(filename, 'r') as file:
+        content = file.read()
+
+    # Use regular expressions to fix indentation
+    fixed_content = re.sub(r'^(\s*)(\w+)\s*=', r'\1  \2 =', content, flags=re.MULTILINE)
+
+    with open(filename, 'w') as file:
+        file.write(fixed_content)
+else:
+    print("Please provide a filename as an argument to this script")
+    sys.exit(1)
+
+
