526411a119d74591ecdda68c968777e5a65f4ec1
Jakob Holst <jho@miracle.dk>
2023-05-15 12:21:21 +0200

    demo

--- fixident.py
+++ fixident.py
@@ -11,7 +11,17 @@ if len(sys.argv) > 1:
 
     # Use regular expressions to fix indentation
     for line in content.splitlines():
-        print(repr(line.replace('^ ', '_')))
+        heading = True
+        newline = ''
+        for char in line:
+            if char == ' ':
+                continue
+            else:
+                heading = False
+                newline = newline + char
+                break
+        print(repr(line))
+        print(repr(newline))
 
     fixed_content = re.sub(r'^(\s*)(\w+)\s*=', r'\1  \2 =', content, flags=re.MULTILINE)
     print(fixed_content)
