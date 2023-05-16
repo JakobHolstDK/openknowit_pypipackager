6edaa7fdd0046df0c16eb0a84b7b5dfa32e76122
Jakob Holst <jho@miracle.dk>
2023-05-16 10:00:19 +0200

    demo view

---
+++ mainview.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = [...]  # Replace [...] with the actual data list
+    return render_template('template.html', data=data)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
