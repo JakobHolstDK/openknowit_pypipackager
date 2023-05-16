ab3db6910b6d0d6e42ccd1df62b9af8356976f41
Jakob Holst <jho@miracle.dk>
2023-05-16 10:47:24 +0200

    demo

--- mainview.py
+++ mainview.py
@@ -19,7 +19,7 @@ def index():
 
 def sort_data(data):
     # Sort the data based on priority
-    sorted_data = sorted(data, key=lambda x: (x['status'], x['name'], x['version']))
+    sorted_data = sorted(data, key=lambda x: (x['status'], x['name'].lower(), x['version']))
     
     return sorted_data
 if __name__ == '__main__':
