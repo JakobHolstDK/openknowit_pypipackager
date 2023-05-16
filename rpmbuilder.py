7c730889a6e885d4502e0fcde127999e30a09142
Jakob Holst <jho@miracle.dk>
2023-05-16 09:39:21 +0200

    replace_setupcfg pulution

--- rpmbuilder.py
+++ rpmbuilder.py
@@ -435,7 +435,6 @@ for package in packages.find(query):
     setupcfg_file = download_folder + package['name'] + '-' + package['version'] + '/setup.cfg'
     setuppy_file = download_folder + package['name'] + '-' + package['version'] + '/setup.py'
     pyprojecttoml_file = download_folder + package['name'] + '-' + package['version'] + '/pyproject.toml'
-replace_setupcfg
     if os.path.exists(setupcfg_file):
       if (not os.path.exists(setuppy_file)) and (not os.path.exists(pyprojecttoml_file)):
         convert_setup_cfg_to_setup_py(setupcfg_file, setuppy_file)
