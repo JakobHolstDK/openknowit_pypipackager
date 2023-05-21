553b1a405d104d70467e8e46f4b3124a19022375
Jakob Holst <jho@miracle.dk>
2023-05-21 21:00:39 +0200

    debbug

--- download.py
+++ download.py
@@ -105,7 +105,7 @@ def downloadpypipackage(name, version):
   before = filenames(download_folder)
   myjob = subprocess.call(["pip", "download", '--no-binary' , ':all:',  "-d", download_folder, package_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   print("--------------------------------------------------------------")
-  pp.pprint(myjob.stdout)
+  pp.pprint(myjob.communicate())
   print("--------------------------------------------------------------")
   
 
