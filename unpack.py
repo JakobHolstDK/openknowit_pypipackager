28bc7a106b5f625a36b346b698b9eb57ea5ab13b
Jakob Holst <jho@miracle.dk>
2023-05-31 13:25:45 +0200

    demo

--- unpack.py
+++ unpack.py
@@ -109,9 +109,9 @@ if __name__ == "__main__":
             packages.update_one(query, update)
             after = filenames(download_folder)
             dif = diflist(before, after)
-            filesadded = []
+            filesadded = ""
             for file in dif:
-              filesadded.append(file) 
+              filesadded  = filesadded + file + " "
             update = {'$set': {'status': 'sourceunpacked', 'sourceunpacked': True, 'sourcepath': filesadded}}
             packages.update_one(query, update)
 
